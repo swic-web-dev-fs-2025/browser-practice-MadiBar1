2978da4de33e74ab96a9f1d86b49ebf70bde32d8
Madison Harris <madison.harris002@student.swic.edu>
2025-11-02 21:26:35 -0600

    added a reset button for click count

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ let clickCount = 0;
 const greetingElement = document.getElementById("greeting");
 const button = document.getElementById("change-greeting");
 const countElement = document.getElementById("click-count");
+const button2 = document.getElementById("reset-count");
 
 function updateGreeting() {
   clickCount++;
@@ -13,4 +14,10 @@ function updateGreeting() {
   countElement.textContent = `Button clicks: ${clickCount}`;
 }
 
+function resetCount() {
+  clickCount = 0;
+  countElement.textContent = `Button clicks: ${clickCount}`;
+}
+
 button.addEventListener("click", updateGreeting);
+button2.addEventListener("click", resetCount);
