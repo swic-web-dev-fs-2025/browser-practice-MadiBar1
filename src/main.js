3b0c4b51c1ccd1c368ee977ad57be9a8a4367219
Madison Harris <madison.harris002@student.swic.edu>
2025-11-03 09:50:25 -0600

    added reset and increment button functions to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
 import "./style.css";
 
 //Variables
+let clickCount = 0;
+
+const countElement = document.getElementById("click-count");
+const button1 = document.getElementById("count-add");
+const button3 = document.getElementById("reset-count");
+
+//Increment Button
+function incrementCount() {
+  clickCount++;
+  countElement.textContent = `Button clicks: ${clickCount}`;
+}
+
+function resetCount() {
+  clickCount = 0;
+  countElement.textContent = `Button clicks: ${clickCount}`;
+}
+
+button1.addEventListener("click", incrementCount);
+button3.addEventListener("click", resetCount);
