c7d60c8812145b8317862ed091562abfb7d770f1
Madison Harris <madison.harris002@student.swic.edu>
2025-11-03 09:42:04 -0600

    Cleared main.js for assignment 1

--- src/main.js
+++ src/main.js
@@ -1,38 +1,3 @@
 import "./style.css";
 
 //Variables
-let clickCount = 0;
-
-const greetingElement = document.getElementById("greeting");
-const button = document.getElementById("change-greeting");
-const countElement = document.getElementById("click-count");
-const button2 = document.getElementById("reset-count");
-const heading = document.querySelector("h1");
-const customGreeting = document.getElementById("custom-greeting");
-
-function updateCustomGreeting() {
-  const newGreeting = customGreeting.value;
-  if (newGreeting.trim() !== "") {
-    greetingElement.textContent = newGreeting;
-  }
-}
-
-function updateGreeting() {
-  clickCount++;
-  greetingElement.textContent = "Hello, Browser JavaScript!";
-  countElement.textContent = `Button clicks: ${clickCount}`;
-  if (clickCount >= 5) {
-    heading.style.color = "red";
-  } else {
-    heading.style.color = "black";
-  }
-}
-
-function resetCount() {
-  clickCount = 0;
-  countElement.textContent = `Button clicks: ${clickCount}`;
-}
-
-button.addEventListener("click", updateGreeting);
-button2.addEventListener("click", resetCount);
-customGreeting.addEventListener("input", updateCustomGreeting);
