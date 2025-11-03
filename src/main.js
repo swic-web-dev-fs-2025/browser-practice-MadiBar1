34036f54e692c20908e0f6f5e45cea4503af163b
Madison Harris <madison.harris002@student.swic.edu>
2025-11-03 10:02:12 -0600

    added decrement button functionality to main.js

--- src/main.js
+++ src/main.js
@@ -5,11 +5,23 @@ let clickCount = 0;
 
 const countElement = document.getElementById("click-count");
 const button1 = document.getElementById("count-add");
+const button2 = document.getElementById("count-subtract");
 const button3 = document.getElementById("reset-count");
 
 //Increment Button
 function incrementCount() {
   clickCount++;
+  countElement.textContent = `Button clicks: ${clickCount}`;
+  if (clickCount >= 10) {
+    document.body.style.backgroundColor = "lightblue";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+//Decrement Button
+function decrementCount() {
+  clickCount--;
   countElement.textContent = `Button clicks: ${clickCount}`;
 }
 
@@ -19,4 +31,5 @@ function resetCount() {
 }
 
 button1.addEventListener("click", incrementCount);
+button2.addEventListener("click", decrementCount);
 button3.addEventListener("click", resetCount);
